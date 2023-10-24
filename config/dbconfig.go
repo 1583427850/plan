0951b05ed72678ca68f3b40d262f45d91617b35d
lin <1583427850@qq.com>
2023-10-24 13:15:56 +0800

    six

--- config/dbconfig.go
+++ config/dbconfig.go
@@ -11,7 +11,7 @@ var DB *gorm.DB
 // 用于连接数据库和初始化数据库等等操作
 func InitMysql() error {
 	var err error
-	dsn := "root:abc8909389@tcp(127.0.0.1:3306)/golearn?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:abc8909389@tcp(47.120.8.78:3306)/golearn?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), nil)
 	if err != nil {
 		panic(err)
